afb601bb58d89563d9602e2a3f6bf4a176fcb3fd
jonas caceres <jonad.caceres@gmail.com>
2020-07-22 18:18:40 -0400

    Final commit

---
+++ index.js
@@ -0,0 +1,196 @@
+// Conexion a firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyAGOBHiDPoofC6oUW4Z2H2PPIS4zzstz_M",
+  authDomain: "example-js-aa7d6.firebaseapp.com",
+  databaseURL: "https://example-js-aa7d6.firebaseio.com",
+  projectId: "example-js-aa7d6",
+  storageBucket: "example-js-aa7d6.appspot.com",
+  messagingSenderId: "767307363369",
+  appId: "1:767307363369:web:1e2e3055de932383a29838",
+};
+var app = firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore(app);
+// helper para crear elemetos y adirle atributos
+function myCreateAttr(element, attributes) {
+  return Object.keys(attributes).reduce((addedAttr, key) => {
+    addedAttr.setAttribute(key, attributes[key]);
+    return addedAttr;
+  }, element);
+}
+// manejador de radioInput
+let team = "teamA";
+const radioInput = (e) => {
+  team = e.target.defaultValue;
+};
+
+function addPlayerToTeam() {
+  const input = document.getElementById("playerName");
+  //   console.log(team);
+  if (input.value != "") {
+    db.collection(team)
+      .add({
+        name: input.value,
+      })
+      .then(function (docRef) {
+        alertAction("success", "Jugador Añadido con exito");
+        console.log("Document written with ID: ", docRef.id);
+      })
+      .catch(function (error) {
+        console.error("Error adding document: ", error);
+      });
+    input.value = "";
+  } else {
+    alertAction("danger", "El nombre no puede estar vacio");
+  }
+}
+// inserta una alerta  con color y mensaje persanalizable
+const players = { teamA: [], teamL: [] };
+function alertAction(type, massage, time = 3000) {
+  players.teamL = [];
+  players.teamA = [];
+  const alert = document.getElementById("alert");
+  const activeAlert = myCreateAttr(document.createElement("div"), {
+    class: `alert alert-${type}`,
+    role: "alert",
+  });
+  activeAlert.innerHTML = massage;
+  alert.appendChild(activeAlert);
+  setTimeout(() => {
+    activeAlert.remove();
+  }, time);
+
+  dataTableUdate("teamL");
+  dataTableUdate();
+}
+// manejador de checkbox
+function playersSelected(e, id) {
+  let playerSelected;
+  const idValue = e.target.id ? e.target.id : id;
+  const value = e.target.value ? e.target.value : e;
+  if (value === "teamA") {
+    playerSelected = players.teamA;
+  } else {
+    playerSelected = players.teamL;
+  }
+  const index = playerSelected.findIndex((id) => id === idValue);
+  if (index === -1) {
+    playerSelected.push(idValue);
+  } else {
+    playerSelected.splice(index, 1);
+  }
+}
+// mover los jugadores seleccionados de un equipo a otro
+function movePlayersSelected(e) {
+  let idsArray, teamLocation, teamC;
+  const addToteam = e.target.value;
+  if (addToteam === "teamA") {
+    idsArray = players.teamL;
+    teamLocation = "teamL";
+  } else {
+    idsArray = players.teamA;
+    teamLocation = "teamA";
+  }
+  //   const collectins = db.collection(e.target.value);
+  idsArray.forEach((id) => {
+    // console.log(idsArray);
+    db.collection(teamLocation)
+      .doc(id)
+      .get()
+      .then((querySnapshot) => {
+        db.collection(addToteam)
+          .add({
+            name: querySnapshot.data().name,
+          })
+          .then(() => {
+            playerRemover(
+              id,
+              idsArray,
+              teamLocation,
+              "jugador/@s transferido/@s",
+              3000
+            );
+          })
+          .catch((error) => {
+            console.log("no se encontro");
+          });
+      });
+  });
+}
+// removedor de jugadores
+function playerRemover(id, data, location, massage, time, type = "success") {
+  let count = data.findIndex((index) => index === id);
+  //   console.log(count);
+  db.collection(location)
+    .doc(id)
+    .delete()
+    .then(() => {
+      if (count >= data.length - 1) {
+        alertAction(type, massage, time);
+      }
+      //   console.log(count, data.length - 1);
+      count++;
+      // console.log("Document successfully deleted!");
+    })
+    .catch((error) => {
+      console.error("Error removing document: ", error);
+    });
+}
+// mover todos los jugadores de u lugar a otro
+function moveAllPlayers(e) {
+  const seurce = e.target.value === "allToA" ? "teamL" : "teamA";
+  const destination = seurce === "teamA" ? "teamL" : "teamA";
+  const data = [];
+  db.collection(seurce)
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        data.push(doc.id);
+        db.collection(destination)
+          .add({
+            name: doc.data().name,
+          })
+          .then((docRef) => {
+            playerRemover(
+              doc.id,
+              data,
+              seurce,
+              `Todos los jugadores se han movido a ${
+                seurce === "teamL" ? "Las Aguilas" : "Licey"
+              }`,
+              3000
+            );
+          });
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+// actuliza la tabla
+function dataTableUdate(team = "teamA") {
+  const idTable = team === "teamA" ? "teamA" : "teamL";
+  db.collection(team)
+    .get()
+    .then((querySnapshot) => {
+      const table = document.getElementById(idTable);
+      table.innerHTML = ``;
+      querySnapshot.forEach((doc) => {
+        table.innerHTML += `
+          <tr>
+              <th scope="row">
+              <input
+                  type="checkbox"
+                  id=${doc.id}
+                  value=${idTable}
+                  onclick=playersSelected(event)
+                  aria-label="Checkbox for following text input"
+              />
+              </th>
+              <td>${doc.data().name}</td>
+          </tr>
+          `;
+      });
+    });
+}
+dataTableUdate("teamL");
+dataTableUdate();
